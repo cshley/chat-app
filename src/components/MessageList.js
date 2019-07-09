2d5730d5683d07daf2c343283043021e9c08b00e
Chloe Ashley <chloeashley193@gmail.com>
2019-07-09 16:08:11 +0100

    dummy data input

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,11 +1,29 @@
 import React from 'react'
 import React, { Component } from 'react'
 
+const DUMMY_DATA = [
+    {
+        senderID: 'chloe',
+        texts: "hellooo"
+    },
+
+    {
+        senderID: 'cal',
+        texts: "wooop"
+    },
+
+    {
+        senderID: 'meg',
+        texts: "heyhey"
+    }
+]
+
+
 class MessageList extends Component {
     render() {
         return(
             <div className="messageList">
-                
+
             </div>
         )
     }
